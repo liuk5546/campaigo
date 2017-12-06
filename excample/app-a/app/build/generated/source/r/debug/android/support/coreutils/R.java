e96a0c8def29dab31d4598c2165f5e87de86d269
LiuKun <702151478@qq.com>
2017-12-06 10:57:34 +0800

    excample commit

--- excample/app-a/app/build/generated/source/r/debug/android/support/coreutils/R.java
+++ excample/app-a/app/build/generated/source/r/debug/android/support/coreutils/R.java
@@ -8,25 +8,25 @@ package android.support.coreutils;
 
 public final class R {
     public static final class attr {
-        public static final int font = 0x7f02006f;
-        public static final int fontProviderAuthority = 0x7f020070;
-        public static final int fontProviderCerts = 0x7f020071;
-        public static final int fontProviderPackage = 0x7f020072;
-        public static final int fontProviderQuery = 0x7f020073;
-        public static final int fontStyle = 0x7f020074;
-        public static final int fontWeight = 0x7f020075;
+        public static final int font = 0x7f03008e;
+        public static final int fontProviderAuthority = 0x7f03008f;
+        public static final int fontProviderCerts = 0x7f030090;
+        public static final int fontProviderPackage = 0x7f030091;
+        public static final int fontProviderQuery = 0x7f030092;
+        public static final int fontStyle = 0x7f030093;
+        public static final int fontWeight = 0x7f030094;
     }
     public static final class id {
-        public static final int italic = 0x7f070039;
-        public static final int normal = 0x7f070044;
+        public static final int italic = 0x7f08004f;
+        public static final int normal = 0x7f080062;
     }
     public static final class styleable {
-        public static final int[] FontFamily = { 0x7f020070, 0x7f020071, 0x7f020072, 0x7f020073 };
+        public static final int[] FontFamily = { 0x7f03008f, 0x7f030090, 0x7f030091, 0x7f030092 };
         public static final int FontFamily_fontProviderAuthority = 0;
         public static final int FontFamily_fontProviderCerts = 1;
         public static final int FontFamily_fontProviderPackage = 2;
         public static final int FontFamily_fontProviderQuery = 3;
-        public static final int[] FontFamilyFont = { 0x7f02006f, 0x7f020074, 0x7f020075 };
+        public static final int[] FontFamilyFont = { 0x7f03008e, 0x7f030093, 0x7f030094 };
         public static final int FontFamilyFont_font = 0;
         public static final int FontFamilyFont_fontStyle = 1;
         public static final int FontFamilyFont_fontWeight = 2;
