eecb3725605ad4187eb051e9b74511d988b64702
Shengyiqun <847719553@qq.com>
2018-01-12 23:19:46 +0800

    最终评审前提交，文档修订
    编写了最终ppt，修订了部分文档。更新代码清单。加入了用户手册的更新。加入项目总结

---
+++ doc/实现阶段/代码清单/com/campaigo/model/PartyRelationShip.java
@@ -0,0 +1,29 @@
+package com.campaigo.model;
+
+public class PartyRelationShip {
+	private String partyRelationShipId;
+	private int partyRelationShipIdCamId;
+	private int role;
+	
+	
+	public String getPartyRelationShipId() {
+		return partyRelationShipId;
+	}
+	public void setPartyRelationShipId(String partyRelationShipId) {
+		this.partyRelationShipId = partyRelationShipId;
+	}
+	public int getPartyRelationShipIdCamId() {
+		return partyRelationShipIdCamId;
+	}
+	public void setPartyRelationShipIdCamId(int partyRelationShipIdCamId) {
+		this.partyRelationShipIdCamId = partyRelationShipIdCamId;
+	}
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	
+	
+}
